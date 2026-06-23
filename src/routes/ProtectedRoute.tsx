04935237a70665034a18d65e1182803945d93dbf
Adarsh Singh <anexus5919@gmail.com>
2026-06-23 22:01:47 +0530

    security: remove dead, insecure ProtectedRoute stubs (#840)

--- src/routes/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
-  const token = localStorage.getItem("token");
-
-  return token ? children : <Navigate to="/login" replace />;
-}
